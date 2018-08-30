5c7324965b381c5946fcb188655410419b93c652
tannt <tannt@omnicasa.com>
2018-08-30 11:09:38 +0700

    - add logger - write log to console - support sent message log to server

--- AutoRegistryClassDDD/App/bootstrapper.ts
+++ AutoRegistryClassDDD/App/bootstrapper.ts
@@ -6,18 +6,22 @@ import store from "./store";
 import App from "./app";
 import AxiosPlugin from "./plugin/http-request";
 
-// import main css
-import "../scss/main.scss";
-import "bootstrap";
+Vue.use(VueRouter);
+Vue.use(AxiosPlugin);
 
-// import service woker
+/** SERVICE WORKER */
 import runtime from "serviceworker-webpack-plugin/lib/runtime";
 if ("serviceWorker" in navigator) {
   const registration = runtime.register();
 }
 
-Vue.use(VueRouter);
-Vue.use(AxiosPlugin);
+/** Logger */
+import "./vue-logger-conf"
+
+
+/** Other Vue configs */
+Vue.config.productionTip = false;
+
 
 /*
     CURRENTLY I HAD COMMENT THIS COMPONENT BECAUSE IT MAKE OUR APPLICATION IS MEMORY LEAK
@@ -26,6 +30,10 @@ Vue.use(AxiosPlugin);
 */
 //import Datatable from "vue2-datatable-component";
 //Vue.use(Datatable);
+
+/** Main App resources */
+import "../scss/main.scss";
+import "bootstrap";
 
 @Component({
   template: "<App/>",
--- AutoRegistryClassDDD/App/plugin/http-request/index.ts
+++ AutoRegistryClassDDD/App/plugin/http-request/index.ts
@@ -1,11 +1,12 @@
 ﻿import _Vue from "vue";
 import Axios from "axios";
 import { AxiosPluginOptions } from "./plugin.conf";
+import * as Logger from 'js-logger';
 
 const AxiosPlugin = {
     install(Vue: typeof _Vue, options?: AxiosPluginOptions) {
         Vue.prototype.$http = Axios;
-        console.log('axios plugin is installing');
+        Logger.log('axios plugin is installing');
     }
 }
 
